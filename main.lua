76aba0b75659cfc8464973943024b07a7740adc9
Winksplorer <oscar.botashev@gmail.com>
2020-12-04 17:44:46 -0800

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,48 @@
+require=os
+
+local version = 2.0
+
+function Glint()
+	while( true )
+	do
+		io.write('>>>')
+		local command = io.read()
+
+		if (command == 'about') then
+			print('Version: ' .. version)
+			print('Rewritten In ' .. _VERSION)
+		end
+
+		if (command == 'help') then
+			print('about - About Glint')
+			print('calc - Calculator')
+			print('help - This Menu')
+			print('cmd - CMD mode')
+		end
+
+		if (command == 'calc') then
+			io.write('Value A: ')
+			local valueA = io.read()
+			io.write('+ or -: ')
+			local mathchoose = io.read()
+			io.write('Value B: ')
+			local valueB = io.read()
+			if (mathchoose == '+') then
+				answer = (valueA + valueB)
+				print('The Answer Is... ' .. answer .. '!')
+			end
+			if (mathchoose == '-') then
+				answer = (valueA - valueB)
+				print('The Answer Is... ' .. answer .. '!')
+			end
+	end
+end
+end
+function startup()
+	local start = os.clock()
+	while os.clock() - start < 1 do end
+	print('Glint ' .. version)
+	print('Type "help" for a list of commands.')
+	Glint()
+end
+startup()
